c38fc57fe3bfdf5e2139e097c304cf32f1278ddc
nadiaschutz <nadiaikon@gmail.com>
2017-07-24 17:25:25 -0400

    add

---
+++ script.js
@@ -0,0 +1,34 @@
+//when i put var city into weatherAsk, everything works..
+
+var weather;
+var api = "http://api.openweathermap.org/data/2.5/weather?q=";
+var apiKey = "&appid=001b0f58045147663b1ea518d34d88b4&";
+var units = "units=metric";
+var city;
+
+
+function setup() {
+    createCanvas(400, 200);
+    document.getElementById("submit").addEventListener("click", weatherAsk);
+}
+
+function weatherAsk() {
+     city = document.getElementById("city").value;
+    var url = api + city + apiKey + units;
+    loadJSON(url, gotData);
+}
+
+function gotData(data) {
+    weather = data;
+
+}
+
+function draw() {
+    background(0);
+    if (weather) {
+        var temp = weather.main.temp;
+        var temp_min = weather.main.temp_min;
+        ellipse(50, 100, temp, temp);
+        ellipse(300, 100, temp_min, temp_min);
+    }
+}
